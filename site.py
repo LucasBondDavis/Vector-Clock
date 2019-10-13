ec955cd4596d7d60cbb1f157251e0bd3691faddb
Lucas Bond Davis <lucasbonddavis@Lucass-MacBook-Pro.local>
2019-10-13 19:26:19 -0400

    small format fix

--- site.py
+++ site.py
@@ -1,6 +1,12 @@
 import socket
 import sys
 
+class eventR(object):
+    def __init__(self, op, time, data):
+        self.op = op
+        self.time = time
+        self.data = data
+
 IP = '127.0.0.1'
 PORT = 8080
 msg = 'Hello World'
@@ -14,6 +20,5 @@ if __name__ == '__main__':
     print('Starting server at IP: {} and PORT: {}'.format(*servaddr))
     while(True):
         data, addr = sock.recvfrom(1024) # 1024 is the buffer size
-        print('received {] bytes'.format(len(data)));
-        print(data)
-        break
+        print('received {} bytes'.format(len(data)));
+        
