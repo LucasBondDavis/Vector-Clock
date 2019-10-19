b920e21c2f590d17445784b5fee280fb1fc3cea3
zrysnd <zrysnd@cardinal-564.dynamic.rpi.edu>
2019-10-19 00:28:19 -0400

    status pending by default

--- reservation.py
+++ reservation.py
@@ -2,4 +2,4 @@ class reservation(object):
     def __init__(self):
         self.client_name_ = ""
         self.list_of_flight_numbers_ = None
-        self.status_pending = True
+        self.status_pending = True #status should be pending when application creates a local reservation
